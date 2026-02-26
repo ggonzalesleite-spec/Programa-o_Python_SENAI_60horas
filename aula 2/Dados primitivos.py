a9e90327711bde63928f499151ecab083f3242a6
ggonzalesleite-spec <ggonzalesleite@gmail.com>
2026-02-26 17:44:30 -0300

    Update Dados primitivos.py

--- aula 2/Dados primitivos.py
+++ aula 2/Dados primitivos.py
@@ -1,7 +1,17 @@
-#dados primitivos em python
+print('ola mundo', 'nome')
+print('resultado da soma:', 2+2)
+print('resultado da divisão', 2/2)
+print('0 aluno passou de ano?', true
+print'altura: ', 1.71)
+print('R$', 500.0)
+
+print('teste', 2 ,5.2, false)
+
+print(1236 , 'teste')#dados primitivos em python
 #tipo string - texto
 
 'texto'
 "texto 2"
 
 #tipo inteiro
+
