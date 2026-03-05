49b638856b9f07db5945d81d3508633db58d12dc
ggonzalesleite-spec <ggonzalesleite@gmail.com>
2026-03-05 16:32:45 -0300

    Add files via upload

---
+++ indentação-refatoração-concatenação/variaveis.py
@@ -0,0 +1,33 @@
+print('resultado', 4*6/2)
+
+print('resultado', 5+7 )
+
+print('resultado', 3**4 )
+
+média = (8 + 12 + 15 ) / 3
+print('resultado', round (média), 2)
+
+print('resultado', 10-2*5 )
+
+print('resultado', 27/3+5 )
+
+
+'---------------------------------------------------'
+
+print('resultado', 9*3 ** 0.5)
+
+resultado = 81 ** 0.5
+print('resultado')
+
+print('resultado', 20+3*4)
+
+print('resultado', 15*2-7)
+
+média = (17 + 21 + 25 ) / 3
+print('resultado', round (média), 2)
+
+print('resultado', 11*2+7)
+
+print('resultado', 15-3*8) 
+
+print('resultado', 2**10)
