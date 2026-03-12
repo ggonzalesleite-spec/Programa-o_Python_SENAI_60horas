06caeaaaff0e7a2a9f882d118b111ba09c69a374
ggonzalesleite-spec <ggonzalesleite@gmail.com>
2026-03-12 17:09:19 -0300

    Add files via upload

---
+++ condicionais/mach_case.py
@@ -0,0 +1,65 @@
+# numero = int(input('numero; '))
+
+# match numero:
+#     case numero if numero % 2 == 0:
+#         print('Par')
+#     case _:
+#         print('impar')    
+
+#-----------------------------------------------------------#
+
+# match numero:
+    
+#     case numero if numero >0:
+#         print('positivo')
+#     case numero if numero <0:
+#         print('negativo')
+#     case _:
+#         print('zero')
+
+
+#-----------------------------------------------------------#
+
+string = input ('texto =')
+
+match string:
+    case():
+        print('vazia')
+    case _:
+        print('cheia')
+
+
+
+
+
+
+
+
+
+
+
+#----------------------------------------------------------#
+
+# match numero:
+   
+#     case x if numero > 10:
+#         ('maior que 10')
+#     case x if numero < 10:
+#         ('menor que 10')
+#     case _:
+#         ('inesistente')
+
+#----------------------------------------------------------#
+
+# idade = int(input('idade:'))
+
+# match idade:
+      
+#       case x if x < 13:
+#             print('criança')
+#       case x if x >14 and x <17:
+#             print('adolecente')
+#       case x if x >18 and x <35:
+#             print('adulto')
+#       case x if x >35 and x <90:
+#             print('idoso')
