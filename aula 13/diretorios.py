27235907338f50cdfdbb33bce17f94e5ccdfb411
ggonzalesleite-spec <ggonzalesleite@gmail.com>
2026-03-27 15:32:19 -0300

    Add files via upload

---
+++ aula 13/diretorios.py
@@ -0,0 +1,64 @@
+# **Exercício 1: Criar e ler um Arquivo**
+
+# Criar e escrever no arquivo
+with open("teste.txt", "w") as arquivo:
+    arquivo.write("Olá, este é um exemplo de arquivo.\n")
+    arquivo.write("Segunda linha do arquivo.")
+
+# Ler o arquivo
+with open("teste.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
+
+
+
+# **Exemplo 2: Cria um Diretório**
+
+
+
+
+
+
+
+
+
+# **Exercício 3: Renomear um Diretório**
+
+
+
+
+
+
+
+
+
+# **Exercício 4:  Listar Arquivos em um Diretório** 
+
+
+
+
+
+
+
+
+
+# **Exercício 5:  Copiar Arquivos em um Diretório**
+
+
+
+
+
+
+
+
+
+# **Exercício 6:  Remover**
+
+
+
+
+
+
+
+
+
